bb040a9df176599234dcb8bc1addb08955465c17
sunilreddyg <sunilreddy.gajjala@gmail.com>
2023-06-19 19:27:27 +0530

    Add files via upload

---
+++ Project_213/src/objectidentification/FB_Login.java
@@ -0,0 +1,24 @@
+package mq.selenium.objectidentification;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FB_Login {
+
+	public static void main(String[] args) {
+		
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://facebook.com");
+		driver.manage().window().maximize();
+		
+		
+		driver.findElement(By.id("email")).sendKeys("darshan");
+		driver.findElement(By.id("pass")).sendKeys("Hello@123");
+		driver.findElement(By.name("login")).click();
+		
+		
+	}
+
+}
---
+++ Project_213/src/objectidentification/Orange_Login.java
@@ -0,0 +1,27 @@
+package mq.selenium.objectidentification;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Orange_Login {
+
+	public static void main(String[] args) throws Exception {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		
+		Thread.sleep(5000);  //Wait for 5 Seconds 
+
+		//Identifying objects with xpath locator builder.
+		
+		//Typing email at  editbox
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+		//Typing password at editbox
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+		//Clicking on login button
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+	}
+
+}
