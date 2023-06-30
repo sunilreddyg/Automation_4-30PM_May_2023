e18dd7f328f7eddb1dfabe3f5bda1a1150e030eb
sunilreddyg <sunilreddy.gajjala@gmail.com>
2023-06-30 18:39:20 +0530

    Add files via upload

---
+++ Project_213/src/mq/selenium/interactions/Mouse/BackButton.java
@@ -0,0 +1,33 @@
+package mq.selenium.interactions.Mouse;
+
+import java.time.Duration;
+import java.util.Collections;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class BackButton {
+
+	public static void main(String[] args) 
+	{
+		
+		   WebDriver driver=new ChromeDriver();
+		   driver.get("https://www.amazon.in");
+		   driver.manage().window().maximize();
+		   driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		
+		     PointerInput mouse = new PointerInput(PointerInput.Kind.MOUSE, "default mouse");
+
+	        Sequence actions = new Sequence(mouse, 0)
+	                .addAction(mouse.createPointerDown(PointerInput.MouseButton.BACK.asArg()))
+	                .addAction(mouse.createPointerUp(PointerInput.MouseButton.BACK.asArg()));
+
+	        ((RemoteWebDriver) driver).perform(Collections.singletonList(actions));
+
+	}
+
+}
---
+++ Project_213/src/mq/selenium/interactions/Mouse/Scroll/ScrollToElement.java
@@ -0,0 +1,31 @@
+package mq.selenium.interactions.Mouse.Scroll;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ScrollToElement {
+
+	public static void main(String[] args) throws Exception 
+	{
+		
+		   WebDriver driver=new ChromeDriver();
+		   driver.get("https://www.amazon.in");
+		   driver.manage().window().maximize();
+		   driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		   
+		  
+		   WebElement BackToTop=driver.findElement(By.xpath("//span[contains(.,'Back to top')]"));
+		   new Actions(driver).scrollToElement(BackToTop).perform();
+		   
+		   WebElement SignInBtn=driver.findElement(By.xpath("(//a[contains(.,'Sign in')])[4]"));
+		   SignInBtn.click();
+		   
+		   //By default scroll object to downside of the page
+		   
+	}
+
+}
